ea9bbe8cecfaf785933fcf142a1ef65a9bba1f57
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-05 22:44:25 +0200

    Add Details page

--- src/app/components/details/Details.js
+++ src/app/components/details/Details.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import './AlbumDetails.scss';
+import TrackList from '../trackList/TrackList';
 
 const Details = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<Fragment>
+			<Link to='/'>
+				<button className='btn btn-primary float-left ml-45'>Back</button>
+			</Link>
+			<div className='container card-group'>
+				<div className='card card-boy'>
+					<TrackList />
+				</div>
+			</div>
+		</Fragment>
+	);
+};
 
-export default Details
+export default Details;
