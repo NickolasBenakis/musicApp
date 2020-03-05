40091598ef58a309a22f5e9f41fcad42ab21635e
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-05 23:00:43 +0200

    Add trackList

---
+++ src/app/components/trackList/TrackList.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { Store } from '../../store/storeContext';
+import Track from '../track/Track';
+
+const TrackList = () => {
+	const { state } = React.useContext(Store);
+
+	return (
+		<Fragment>
+			<ul className='list-group'>
+				{state.currentAlbum.tracks.map((track, index) => {
+					return (
+						<Track
+							key={index}
+							track={track}
+							albumPhoto={state.currentAlbum.photo}
+						/>
+					);
+				})}
+			</ul>
+		</Fragment>
+	);
+};
+
+export default TrackList;
