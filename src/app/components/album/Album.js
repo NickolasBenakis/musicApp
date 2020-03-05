81e3cacfc6e9e8548f6503a2d5d84a0a7c2cd604
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-05 22:28:38 +0200

    Add album components

--- src/app/components/album/Album.js
+++ src/app/components/album/Album.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Store } from '../../store/storeContext';
+import { Link } from 'react-router-dom';
+import './Album.scss';
 
-const Album = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Album = ({ album }) => {
+	const { dispatch } = React.useContext(Store);
 
-export default Album
+	const handleClick = e => {
+		dispatch({
+			type: 'SELECT_ALBUM',
+			payload: album.id
+		});
+	};
+	return (
+		<Fragment>
+			<div className='card album'>
+				<img src={album.photo} className='card-img-top' alt='album_photo' />
+				<div className='card-body'>
+					<h5 className='card-title'>{album.title}</h5>
+					<Link
+						to={`details?id=${album.id}`}
+						className='btn btn-primary'
+						onClick={handleClick}>
+						More details
+					</Link>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default Album;
--- src/app/components/albumList/AlbumList.js
+++ src/app/components/albumList/AlbumList.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import Album from '../album/Album';
+import { Store } from '../../store/storeContext';
 
 const AlbumList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	const { state } = React.useContext(Store);
 
-export default AlbumList
+	return (
+		<Fragment>
+			{state.albums.map((album, index) => {
+				return <Album key={index} album={album} />;
+			})}
+		</Fragment>
+	);
+};
+
+export default AlbumList;
