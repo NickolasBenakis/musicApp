8b36afe859a1d63eac06869765a1af4a6b29b65f
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-05 22:15:13 +0200

    Fix import issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './theme/index.scss';
-import App from './App';
+import App from '../src/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
