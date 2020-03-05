17d1105c004cba57581ed1ca5cd1c255c1b2fda4
nickolasBenakis <nickolasbenele@gmail.com>
2020-03-05 21:14:30 +0200

    Add pure components, project structure

---
+++ src/api/albums.js
@@ -0,0 +1,95 @@
+export const ALBUMS = [
+	{
+		id: 'A0',
+		title: 'Heroes',
+		artist: 'Nick',
+		photo:
+			'https://res.cloudinary.com/nickolasben/image/upload/v1583335642/music/lox75vwtbwdsoevwyihl.jpg',
+		tracks: [
+			{
+				id: 'T0',
+				index: 0,
+				title: 'song0',
+				artist: 'Nick',
+				genre: 'Rock',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335642/music/lox75vwtbwdsoevwyihl.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/In%20Flames%20-%20Take%20This%20Life%20(Official%20Video).mp3?alt=media&token=be156589-aab4-484a-a260-b1c66408a689'
+			},
+			{
+				id: 'T1',
+				index: 1,
+				title: 'song1',
+				artist: 'Nick',
+				genre: 'Rock',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335642/music/lox75vwtbwdsoevwyihl.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/Mariah%20Carey%20-%20All%20I%20Want%20for%20Christmas%20Is%20You%20(Make%20My%20Wish%20Come%20True%20Edition).mp3?alt=media&token=f61fb2c6-a092-4414-8a45-541e60dd28bc'
+			}
+		]
+	},
+	{
+		id: 'A1',
+		title: 'Heroes1',
+		artist: 'David',
+		photo:
+			'https://res.cloudinary.com/nickolasben/image/upload/v1583335553/music/s36nxjzylccibfwfklc1.jpg',
+		tracks: [
+			{
+				id: 'T444',
+				index: 0,
+				title: 'song0',
+				artist: 'David',
+				genre: 'Rock',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335553/music/s36nxjzylccibfwfklc1.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/Mariah%20Carey%20-%20All%20I%20Want%20for%20Christmas%20Is%20You%20(Make%20My%20Wish%20Come%20True%20Edition).mp3?alt=media&token=f61fb2c6-a092-4414-8a45-541e60dd28bc'
+			},
+			{
+				id: 'T555',
+				index: 1,
+				title: 'song1',
+				artist: 'David',
+				genre: 'Rock',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335553/music/s36nxjzylccibfwfklc1.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/Mariah%20Carey%20-%20All%20I%20Want%20for%20Christmas%20Is%20You%20(Make%20My%20Wish%20Come%20True%20Edition).mp3?alt=media&token=f61fb2c6-a092-4414-8a45-541e60dd28bc'
+			},
+			{
+				id: 'T666',
+				index: 2,
+				title: 'song2',
+				artist: 'David',
+				genre: 'Rock',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335553/music/s36nxjzylccibfwfklc1.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/In%20Flames%20-%20Take%20This%20Life%20(Official%20Video).mp3?alt=media&token=be156589-aab4-484a-a260-b1c66408a689'
+			}
+		]
+	},
+	{
+		id: 'A2',
+		title: 'JazzIt',
+		artist: 'JazzMan',
+		photo:
+			'https://res.cloudinary.com/nickolasben/image/upload/v1583335630/music/wk4pjdpixfqjmj5fnv0p.jpg',
+		tracks: [
+			{
+				id: 'T30',
+				index: 0,
+				title: 'song1',
+				artist: 'Nick',
+				genre: 'Jazz',
+				photo:
+					'https://res.cloudinary.com/nickolasben/image/upload/v1583335630/music/wk4pjdpixfqjmj5fnv0p.jpg',
+				source:
+					'https://firebasestorage.googleapis.com/v0/b/music-31b94.appspot.com/o/In%20Flames%20-%20Take%20This%20Life%20(Official%20Video).mp3?alt=media&token=be156589-aab4-484a-a260-b1c66408a689'
+			}
+		]
+	}
+];
---
+++ src/app/components/albumList/AlbumList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlbumList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AlbumList
